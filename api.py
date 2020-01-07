693621d78aeba9d1f2e9a8cbdba87aad8a4464bd
ysgo <wjion8428@gmail.com>
2020-01-07 22:09:48 +0900

    update flask-restful api

--- api.py
+++ api.py
@@ -1,16 +1,63 @@
-from flask import Flask
-from flask_restful import Resource, Api
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse, abort
 
 app = Flask(__name__)
 api = Api(app)
 
+TODOS = {
+    'todo1': {'task': 'create an API'},
+    'todo2': {'task': 'update an API'},
+    'todo3': {'task': 'do you understand?'},
+}
 
-class HelloWorld(Resource):
+
+def abort_if_todo_doesnt_exist(todo_id):
+    if todo_id not in TODOS:
+        abort(404, message="Todo {} doesn't exist".format(todo_id))
+
+
+parser = reqparse.RequestParser()
+parser.add_argument('task')
+
+
+@app.route('/')
+def main():
+    return 'Basic Root Start'
+
+
+class Todo(Resource):
+    def get(self, todo_id):
+        abort_if_todo_doesnt_exist(todo_id)
+        return TODOS[todo_id]
+
+    def put(self, todo_id):
+        args = parser.parse_args()
+        task = {'task': args['task']}
+        TODOS[todo_id] = task
+        return task, 201
+
+    def delete(self, todo_id):
+        abort_if_todo_doesnt_exist(todo_id)
+        del TODOS[todo_id]
+        return '', 204
+
+
+class TodoList(Resource):
     def get(self):
-        return {'hello': 'world'}
+        return TODOS
+
+    def post(self):
+        args = parser.parse_args()
+        todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
+        todo_id = 'todo%i' % todo_id
+        TODOS[todo_id]  = {'task': args['task']}
+        return TODOS[todo_id], 201
 
 
-api.add_resource(HelloWorld, '/')
+api.add_resource(TodoList, '/todos')
+api.add_resource(Todo, '/todos/<todo_id>')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.getenv('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
