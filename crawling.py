c69febab9876e0aeef0f9692cb9c7c935867010e
ysgo <wjion8428@gmail.com>
2020-01-09 00:39:11 +0900

    add basic crawling data

--- crawling.py
+++ crawling.py
@@ -17,11 +17,34 @@ browser.implicitly_wait(2)
 
 browser.get('https://www.bigkinds.or.kr/v2/news/search.do')
 
-menu_btn = browser.find_element_by_css_selector('#collapse-step-2 > div > div > div.col-sm-9.col-lg-10 > div.row > div.col-xs-12.col-lg-8.col-sm-8 > ul > li.active > a')
-name = browser.find_element(By.XPATH, '/html/body/div[9]/div/h1')
+category = browser.find_element_by_xpath('//*[@id="filter-category"]/div/div[1]/label')
+category_btn = browser.find_element_by_xpath('//*[@id="filter-category-001000000"]')
+category_btn.click()
 
-print(menu_btn.text)
-print(name.text)
+time.sleep(4)
+newsname = browser.find_element_by_xpath('//*[@id="news-results"]/div[1]/div[2]/div[2]/a')
+title_hrf = browser.find_element_by_xpath('//*[@id="news-results"]/div[1]/div[2]/h4')
+title_hrf.click()
 
-time.sleep(1)
+time.sleep(2)
+
+title = browser.find_element_by_xpath('//*[@id="myModalLabel"]')
+date = browser.find_element_by_xpath('//*[@id="news-detail-modal"]/div/div/div[1]/div[2]/span[4]')
+author = browser.find_element_by_xpath('//*[@id="news-detail-modal"]/div/div/div[1]/div[2]/span[5]')
+img_url = browser.find_element_by_xpath('//*[@id="news-detail-modal"]/div/div/div[2]/div/div/img')
+
+contents = browser.find_element_by_xpath('//*[@id="news-detail-modal"]/div/div/div[2]/div')
+
+time.sleep(2)
+
+close_btn = browser.find_element_by_xpath('//*[@id="news-detail-modal"]/div/div/div[1]/button/span')
+close_btn.click()
+
+print(newsname.text)
+print(category)
+print(title)
+print(date.text)
+print(author)
+print(contents.text)
+
 browser.quit()
