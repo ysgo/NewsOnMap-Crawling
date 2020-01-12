49d81f4fc455b36327bd4c2c895cb64ec260dc13
ysgo <wjion8428@gmail.com>
2020-01-12 23:18:52 +0900

    상세뉴스 사진링크, 본문, 닫기 버튼 클릭 이벤트

--- crawling.py
+++ crawling.py
@@ -16,7 +16,7 @@ browser = webdriver.Chrome('chromedriver', chrome_options=chrome_options, servic
 browser.implicitly_wait(2)
 
 browser.get('https://www.bigkinds.or.kr/v2/news/search.do')
-id=None; newsname=None; title=None; category=None; date=None; url=None; content=None; dataSize=None
+category=None; newsname=None; title=None; date=None; url=None; content=None; dataSize=None
 print('getElementText is Start')
 try:
     for menu in range(4, 5):
@@ -27,6 +27,7 @@ try:
         print(menu)
         category_btn = browser.find_element_by_css_selector(category_btn_link)
         category_btn.click()
+        time.sleep(3)
 
         category_link = '#filter-category > div > div:nth-child(' + str(menu) + ') > label'
         category = browser.find_element_by_css_selector(category_link).text
@@ -36,27 +37,44 @@ try:
         page = 4
         for pageNB in range(1, 2):
             for i in range(1, 2):
-                print(pageNB);print(i)
+                print('pageNB :' + pageNB + ', i :' + i)
                 newsname_link = '#news-results > div:nth-child(' + str(i) + ') > div.news-item__body > div.news-item__meta > a'
                 newsname = browser.find_element_by_css_selector(newsname_link).text
-                print(newsname)
 
                 date_link = '#news-results > div:nth-child(' + str(i) + ') > div.news-item__body > div.news-item__meta > span.news-item__date'
                 date = browser.find_element_by_css_selector(date_link).text
-                print(date)
 
                 title_link =  '#news-results > div:nth-child(' + str(i) + ') > div.news-item__body > h4'
                 title_elm = browser.find_element_by_css_selector(title_link)
                 title = title_elm.text
-                print(title)
                 title_elm.click()
+                time.sleep(3)
 
                 ## News Detail Modal Open ##
+                img_link = '#news-detail-modal > div > div > div.modal-body > div > div > img'
+                img_elm = browser.find_element_by_css_selector(img_link)
+                url = img_elm.get_attribute('src')
+
+                content_link = '#news-detail-modal > div > div > div.modal-body > div'
+                content = browser.find_element_by_css_selector(content_link).text
+                time.sleep(2)
+
+                close_btn_link = '#news-detail-modal > div > div > div.modal-header > button > span'
+                close_btn = browser.find_element_by_css_selector(close_btn_link)
+                close_btn.click()
+                time.sleep(2)
+
+                print('category :' + category)
+                print('newsname :' + newsname)
+                print('title :' + title)
+                print('date :' + date)
+                print('url :' + url)
+                print('content :' + content)
 except:
-    print('d')
+    print('exception interrupt')
 else:
-    print('e')
+    print('else exception')
 finally:
-    print('z')
+    print('final execute')
 
 browser.quit()
