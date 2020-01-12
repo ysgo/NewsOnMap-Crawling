af837d3da5e7892cae122d2e03208f0bdaedb267
ysgo <wjion8428@gmail.com>
2020-01-13 01:06:55 +0900

    news crawling result complete by pandas dataframe

--- crawling.py
+++ crawling.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-import time
+import pandas as pd
+import time, sys
 
 startTime = time.time()
 
@@ -14,10 +14,9 @@ chrome_options.add_argument('lang=ko_KR')
 chrome_options.add_argument('--log-level=3')
 
 browser = webdriver.Chrome('chromedriver', chrome_options=chrome_options, service_log_path='NUL')
-# browser.implicitly_wait(2)
-
 browser.get('https://www.bigkinds.or.kr/v2/news/search.do')
-category=None; newsname=None; title=None; date=None; url=None; content=None; dataSize=None
+# browser.implicitly_wait(2)
+list_category=[]; list_newsname=[]; list_title=[]; list_date=[]; list_url=[]; list_content=[]; list_dataSize=[]
 print('getElementText is Start')
 try:
     for menu in range(4, 5):
@@ -34,7 +33,7 @@ try:
 
         page = 4
         for pageNB in range(1, 2):
-            for i in range(1, 3):
+            for i in range(1, 2):
                 print('pageNB : ' + str(pageNB))
                 print('i : ' + str(i))
                 newsname_link = '#news-results > div:nth-child(' + str(i) + ') > div.news-item__body > div.news-item__meta > a'
@@ -43,7 +42,7 @@ try:
                 date_link = '#news-results > div:nth-child(' + str(i) + ') > div.news-item__body > div.news-item__meta > span.news-item__date'
                 date = browser.find_element_by_css_selector(date_link).text
 
-                title_link =  '#news-results > div:nth-child(' + str(i) + ') > div.news-item__body > h4'
+                title_link = '#news-results > div:nth-child(' + str(i) + ') > div.news-item__body > h4'
                 title_elm = browser.find_element_by_css_selector(title_link)
                 title = title_elm.text
                 title_elm.click()
@@ -51,9 +50,11 @@ try:
 
                 ## News Detail Modal Open ##
                 img_link = '#news-detail-modal > div > div > div.modal-body > div > div > img'
-                img_elm = browser.find_element_by_css_selector(img_link)
-                url = img_elm.get_attribute('src')
 
+                img_elm = browser.find_elements_by_css_selector(img_link)
+                url = str(0)
+                if len(img_elm) != 0:
+                    url = img_elm[0].get_attribute('src')
                 content_link = '#news-detail-modal > div > div > div.modal-body > div'
                 content = browser.find_element_by_css_selector(content_link).text
                 time.sleep(2)
@@ -63,16 +64,26 @@ try:
                 close_btn.click()
                 time.sleep(2)
 
+                dataSize = sys.getsizeof([category, newsname, title, date, url, content])
+
                 print('category : ' + category)
                 print('newsname : ' + newsname)
                 print('title : ' + title)
                 print('date : ' + date)
                 print('url : ' + url)
                 print('content : ' + content)
+                print('dataSize: ' + str(dataSize))
+
+                list_category.append(category)
+                list_newsname.append(newsname)
+                list_title.append(title)
+                list_date.append(date)
+                list_url.append(url)
+                list_content.append(content)
+                list_dataSize.append(dataSize)
 
             if page == 4:
                 break
-
             page_btn_link = '#news-results-pagination > ul > li:nth-child(' + str(page) + ') > a'
             page_btn = browser.find_element_by_css_selector(page_btn_link)
             page_btn.click()
@@ -92,6 +103,18 @@ try:
         time.sleep(5)
 
     ## Dataframe by Pandas
+    result_data = {
+        'category': list_category,
+        'newsname': list_newsname,
+        'title': list_title,
+        'date': list_date,
+        'url': list_url,
+        'content': list_content,
+        'dataSize': list_dataSize
+    }
+    result = pd.DataFrame(result_data)
+    print(result)
+
 except:
     print('exception interrupt')
     pass
