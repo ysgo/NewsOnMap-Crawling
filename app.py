560706b770ede5275c36c42121878b462737f1cc
ysgo <wjion8428@gmail.com>
2020-01-13 23:10:48 +0900

    crawling start api and get json data

--- app.py
+++ app.py
@@ -1,61 +1,20 @@
-from flask import Flask, request
-from flask_restful import Resource, Api, reqparse, abort
+from flask import Flask
+from crawling import startCrawling
+from flask_cors import CORS
 
 app = Flask(__name__)
-api = Api(app)
-
-TODOS = {
-    'todo1': {'task': 'create an API'},
-    'todo2': {'task': 'update an API'},
-    'todo3': {'task': 'do you understand?'},
-}
+CORS(app)
 
 
-def abort_if_todo_doesnt_exist(todo_id):
-    if todo_id not in TODOS:
-        abort(404, message="Todo {} doesn't exist".format(todo_id))
+@app.route('/news/crawling')
+def index():
+    get_news = startCrawling()
+    if get_news is None:
+        return 'empty', 200
+    else:
+        get_news.to_json(orient='index', force_ascii=False)
+        return get_news.to_json(orient='index', force_ascii=False)
 
-
-parser = reqparse.RequestParser()
-parser.add_argument('task')
-
-
-@app.route('/')
-def main():
-    return 'Basic Root Start'
-
-
-class Todo(Resource):
-    def get(self, todo_id):
-        abort_if_todo_doesnt_exist(todo_id)
-        return TODOS[todo_id]
-
-    def put(self, todo_id):
-        args = parser.parse_args()
-        task = {'task': args['task']}
-        TODOS[todo_id] = task
-        return task, 201
-
-    def delete(self, todo_id):
-        abort_if_todo_doesnt_exist(todo_id)
-        del TODOS[todo_id]
-        return '', 204
-
-
-class TodoList(Resource):
-    def get(self):
-        return TODOS
-
-    def post(self):
-        args = parser.parse_args()
-        todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
-        todo_id = 'todo%i' % todo_id
-        TODOS[todo_id]  = {'task': args['task']}
-        return TODOS[todo_id], 201
-
-
-api.add_resource(TodoList, '/todos')
-api.add_resource(Todo, '/todos/<todo_id>')
 
 if __name__ == '__main__':
     import os
--- crawling.py
+++ crawling.py
@@ -1,128 +1,128 @@
-from selenium import webdriver
-import pandas as pd
-import time, sys
+def startCrawling():
+    from selenium import webdriver
+    import pandas as pd
+    import time, sys, re
 
-startTime = time.time()
+    startTime = time.time()
 
-# chromedriver version 79.0
-# chrome headless mode options
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--disable-gpu')
-chrome_options.add_argument('lang=ko_KR')
-# chrome_options.add_argument('--disable-logging')
-chrome_options.add_argument('--log-level=3')
+    # chromedriver version 79.0
+    # chrome headless mode options
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('lang=ko_KR')
+    chrome_options.add_argument('--log-level=3')
 
-browser = webdriver.Chrome('chromedriver', chrome_options=chrome_options, service_log_path='NUL')
-browser.get('https://www.bigkinds.or.kr/v2/news/search.do')
-# browser.implicitly_wait(2)
-list_category=[]; list_newsname=[]; list_title=[]; list_date=[]; list_url=[]; list_content=[]; list_dataSize=[]
-print('getElementText is Start')
-try:
-    for menu in range(4, 5):
-        if menu == 4:
-            menu = 6
-        category_btn_link = '#filter-category-00' + str(menu) + '000000'
-        category_btn = browser.find_element_by_css_selector(category_btn_link)
-        category_btn.click()
-        time.sleep(3)
+    browser = webdriver.Chrome('chromedriver', chrome_options=chrome_options, service_log_path='NUL')
+    browser.get('https://www.bigkinds.or.kr/v2/news/search.do')
+    list_category=[]; list_newsname=[]; list_title=[]; list_date=[]; list_url=[]; list_content=[]; list_dataSize=[]
+    print('getElementText is Start')
+    try:
+        for menu in range(4, 5):
+            if menu == 4:
+                menu = 6
+            category_btn_link = '#filter-category-00' + str(menu) + '000000'
+            category_btn = browser.find_element_by_css_selector(category_btn_link)
+            category_btn.click()
+            time.sleep(3)
 
-        category_link = '#filter-category > div > div:nth-child(' + str(menu) + ') > label'
-        category = browser.find_element_by_css_selector(category_link).text
-        category = category[:2]
+            category_link = '#filter-category > div > div:nth-child(' + str(menu) + ') > label'
+            category = browser.find_element_by_css_selector(category_link).text
+            category = category[:2]
 
-        page = 4
-        for pageNB in range(1, 2):
-            for i in range(1, 2):
-                print('pageNB : ' + str(pageNB))
-                print('i : ' + str(i))
-                newsname_link = '#news-results > div:nth-child(' + str(i) + ') > div.news-item__body > div.news-item__meta > a'
-                newsname = browser.find_element_by_css_selector(newsname_link).text
+            page = 4
+            for pageNB in range(1, 2):
+                for i in range(1, 3):
+                    print('pageNB : ' + str(pageNB))
+                    print('i : ' + str(i))
+                    newsname_link = '#news-results > div:nth-child(' + str(i) + ') > div.news-item__body > div.news-item__meta > a'
+                    newsname = browser.find_element_by_css_selector(newsname_link).text
 
-                date_link = '#news-results > div:nth-child(' + str(i) + ') > div.news-item__body > div.news-item__meta > span.news-item__date'
-                date = browser.find_element_by_css_selector(date_link).text
+                    date_link = '#news-results > div:nth-child(' + str(i) + ') > div.news-item__body > div.news-item__meta > span.news-item__date'
+                    date = browser.find_element_by_css_selector(date_link).text
 
-                title_link = '#news-results > div:nth-child(' + str(i) + ') > div.news-item__body > h4'
-                title_elm = browser.find_element_by_css_selector(title_link)
-                title = title_elm.text
-                title_elm.click()
-                time.sleep(3)
+                    title_link = '#news-results > div:nth-child(' + str(i) + ') > div.news-item__body > h4'
+                    title_elm = browser.find_element_by_css_selector(title_link)
+                    title = title_elm.text
+                    title_elm.click()
+                    time.sleep(2)
 
-                ## News Detail Modal Open ##
-                img_link = '#news-detail-modal > div > div > div.modal-body > div > div > img'
+                    ## News Detail Modal Open ##
+                    img_link = '#news-detail-modal > div > div > div.modal-body > div > div > img'
 
-                img_elm = browser.find_elements_by_css_selector(img_link)
-                url = str(0)
-                if len(img_elm) != 0:
-                    url = img_elm[0].get_attribute('src')
-                content_link = '#news-detail-modal > div > div > div.modal-body > div'
-                content = browser.find_element_by_css_selector(content_link).text
-                time.sleep(2)
+                    img_elm = browser.find_elements_by_css_selector(img_link)
+                    url = str(0)
+                    if len(img_elm) != 0:
+                        url = img_elm[0].get_attribute('src')
+                    content_link = '#news-detail-modal > div > div > div.modal-body > div'
+                    content = browser.find_element_by_css_selector(content_link).text
+                    content = re.sub('\n', '<br>', content)
+                    time.sleep(2)
 
-                close_btn_link = '#news-detail-modal > div > div > div.modal-header > button > span'
-                close_btn = browser.find_element_by_css_selector(close_btn_link)
-                close_btn.click()
-                time.sleep(2)
+                    close_btn_link = '#news-detail-modal > div > div > div.modal-header > button > span'
+                    close_btn = browser.find_element_by_css_selector(close_btn_link)
+                    close_btn.click()
+                    time.sleep(2)
 
-                dataSize = sys.getsizeof([category, newsname, title, date, url, content])
+                    dataSize = sys.getsizeof([category, newsname, title, date, url, content])
 
-                print('category : ' + category)
-                print('newsname : ' + newsname)
-                print('title : ' + title)
-                print('date : ' + date)
-                print('url : ' + url)
-                print('content : ' + content)
-                print('dataSize: ' + str(dataSize))
+                    print('category : ' + category)
+                    print('newsname : ' + newsname)
+                    print('title : ' + title)
+                    print('date : ' + date)
+                    print('url : ' + url)
+                    print('content : ' + content)
+                    print('dataSize: ' + str(dataSize))
 
-                list_category.append(category)
-                list_newsname.append(newsname)
-                list_title.append(title)
-                list_date.append(date)
-                list_url.append(url)
-                list_content.append(content)
-                list_dataSize.append(dataSize)
+                    list_category.append(category)
+                    list_newsname.append(newsname)
+                    list_title.append(title)
+                    list_date.append(date)
+                    list_url.append(url)
+                    list_content.append(content)
+                    list_dataSize.append(dataSize)
 
-            if page == 4:
+                if page == 4:
+                    break
+                page_btn_link = '#news-results-pagination > ul > li:nth-child(' + str(page) + ') > a'
+                page_btn = browser.find_element_by_css_selector(page_btn_link)
+                page_btn.click()
+                time.sleep(5)
+                if page == 10:
+                    page = 4
+                else:
+                    page += 1
+
+            if menu == 6:
                 break
-            page_btn_link = '#news-results-pagination > ul > li:nth-child(' + str(page) + ') > a'
-            page_btn = browser.find_element_by_css_selector(page_btn_link)
-            page_btn.click()
+            # browser.execute_script('scrollBy(0, 250);')
+            # time.sleep(2)
+            reset_btn_link = '#collapse-step-2 > div > div > div.col-sm-3.col-lg-2 > div > h4 > button > i'
+            reset_btn = browser.find_element_by_css_selector(reset_btn_link)
+            reset_btn.click()
             time.sleep(5)
-            if page == 10:
-                page = 4
-            else:
-                page += 1
 
-        if menu == 6:
-            break
-        # browser.execute_script('scrollBy(0, 250);')
-        # time.sleep(2)
-        reset_btn_link = '#collapse-step-2 > div > div > div.col-sm-3.col-lg-2 > div > h4 > button > i'
-        reset_btn = browser.find_element_by_css_selector(reset_btn_link)
-        reset_btn.click()
-        time.sleep(5)
+        ## Dataframe by Pandas
+        result_data = {
+            'category': list_category,
+            'newsname': list_newsname,
+            'title': list_title,
+            'date': list_date,
+            'url': list_url,
+            'content': list_content,
+            'dataSize': list_dataSize
+        }
+        result = pd.DataFrame(result_data)
+    except:
+        print('exception interrupt')
+        result = None
+        pass
+    else:
+        print('getElement is Finish')
+    finally:
+        endTime = time.time()
+        print('StartTime : ' + str(startTime) + ', EndTime : ' + str(endTime))
+        print('Total execution time estimate : ' + str(endTime - startTime))
+        browser.quit()
+    return result
 
-    ## Dataframe by Pandas
-    result_data = {
-        'category': list_category,
-        'newsname': list_newsname,
-        'title': list_title,
-        'date': list_date,
-        'url': list_url,
-        'content': list_content,
-        'dataSize': list_dataSize
-    }
-    result = pd.DataFrame(result_data)
-    print(result)
-
-except:
-    print('exception interrupt')
-    pass
-else:
-    print('else exception')
-finally:
-    print('getElement is Finish')
-    endTime = time.time()
-    print('StartTime : ' + str(startTime) + ', EndTime : ' + str(endTime))
-    print('Total execution time estimate : ' + str(endTime - startTime))
-    browser.quit()
